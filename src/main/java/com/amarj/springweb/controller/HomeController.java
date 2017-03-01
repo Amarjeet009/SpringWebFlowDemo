6c47e3c762ebb6fdedcc9a164e6beb70577f6704
amarjeet009 <amarjeetrao@gmail.com>
2017-03-02 03:53:54 +0530

    Spring Web flow
    Spring Web Flow builds on Spring MVC and allows implementing the "flows"
    of a web application. A flow encapsulates a sequence of steps that guide
    a user through the execution of some business task. It spans multiple
    HTTP requests, has state, deals with transactional data, is reusable,
    and may be dynamic and long-running in nature..

---
+++ src/main/java/com/amarj/springweb/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.amarj.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class HomeController {
+	@RequestMapping("index")
+	public String home(){
+		return "index";
+	}
+
+}
